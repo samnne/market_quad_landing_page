76e7a4efe6681f07b5770d0bf7fabdff11cca59a
Samuel Echefu <mashce@uvic.ca>
2026-04-22 12:10:25 -0700

    added llms.txt .json and licenses

---
+++ src/sections/Licenses.tsx
@@ -0,0 +1,14 @@
+import Navbar from '@/components/Navbar'
+
+const Licenses = () => {
+  return (
+    <>
+    <Navbar />
+    <section className='bg-white min-h-screen flex justify-center items-center text-6xl sm:text-8xl'>
+        <h1 className='font-bold text-pretty text-primary  '>Coming Soon</h1>
+    </section>
+    </>
+  )
+}
+
+export default Licenses
